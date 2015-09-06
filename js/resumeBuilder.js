f7948a891f5c9980f2a802fc70fb1de8d5b60d08
Milan Fort <milan.fort@gmail.com>
2015-09-06 19:34:10 +0200

    Switched to Handlebars templating instead of using replace calls.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -17,43 +17,13 @@ var bio = {
 };
 
 bio.display = function () {
-    var $header = $('#header');
-    var $topContacts = $("#topContacts");
-    var $footerContacts = $("#footerContacts");
+    var template = Handlebars.compile($("#headerTemplate").html());
+    var header = template(this);
+    $('#header').append(header);
 
-    var name = HTMLheaderName.replace("%data%", bio.name);
-    var role = HTMLheaderRole.replace("%data%", bio.role);
-
-    var mobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
-    var email = HTMLemail.replace('%data%', bio.contacts.email);
-    var github = HTMLgithub.replace('%data%', bio.contacts.github);
-    var twitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
-    var location = HTMLlocation.replace('%data%', bio.contacts.location);
-
-    $header.prepend(role);
-    $header.prepend(name);
-
-    $topContacts.append(mobile);
-    $topContacts.append(email);
-    $topContacts.append(github);
-    $topContacts.append(twitter);
-    $topContacts.append(location);
-
-    $footerContacts.append(mobile);
-    $footerContacts.append(email);
-    $footerContacts.append(github);
-    $footerContacts.append(twitter);
-    $footerContacts.append(location);
-
-    $header.append(HTMLbioPic.replace('%data%', bio.biopic));
-    $header.append(HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage));
-
-    if (bio.skills) {
-        $header.append(HTMLskillsStart);
-        bio.skills.forEach(function (skill) {
-            $("#skills").append(HTMLskills.replace("%data%", skill));
-        });
-    }
+    template = Handlebars.compile($("#contactsTemplate").html());
+    var contactEntries = template(this);
+    $('#lets-connect').append(contactEntries);
 };
 
 
@@ -79,21 +49,9 @@ var work = {
 };
 
 work.display = function () {
-    for (var jobIndex in work.jobs) {
-        $("#workExperience").append(HTMLworkStart);
-        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[jobIndex].employer);
-        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[jobIndex].title);
-        var formattedEmployerTitle = formattedEmployer + formattedTitle;
-
-        var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[jobIndex].dates);
-        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[jobIndex].location);
-        var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[jobIndex].description);
-
-        $(".work-entry:last").append(formattedEmployerTitle);
-        $(".work-entry:last").append(formattedWorkDates);
-        $(".work-entry:last").append(formattedLocation);
-        $(".work-entry:last").append(formattedWorkDescription);
-    }
+    var template = Handlebars.compile($("#workExperienceTemplate").html());
+    var workEntries = template(this);
+    $("#workExperience").append(workEntries);
 };
 
 
@@ -117,18 +75,9 @@ var projects = {
 };
 
 projects.display = function() {
-    for (var i = 0; i < this.projects.length; i++) {
-        var project = this.projects[i];
-        var $start = $(HTMLprojectStart);
-        $start.append(HTMLprojectTitle.replace("%data%", project.title));
-        $start.append(HTMLprojectDates.replace("%data%", project.dates));
-        $start.append(HTMLprojectDescription.replace("%data%", project.description));
-
-        for (var j = 0; j < project.images.length; j++) {
-            $start.append(HTMLprojectImage.replace("%data%", project.images[j]));
-        }
-        $("#projects").append($start);
-    }
+    var template = Handlebars.compile($("#projectsTemplate").html());
+    var projectEntries = template(this);
+    $('#projects').append(projectEntries);
 };
 
 
@@ -140,7 +89,7 @@ var education = {
             "name": "Massachusetts Institute of Technology",
             "location": "Cambridge, MA, US",
             "degree": "M.Sc.",
-            "majors" : ["Software Engineering"],
+            "majors" : ["Software Engineering", "Data Science"],
             "dates": "2005-2009",
             "url": "http://web.mit.edu"
         },
@@ -176,41 +125,9 @@ var education = {
 };
 
 education.display = function () {
-    var $education = $('#education'),
-        i;
-
-    if (education.schools) {
-        for (i = 0; i < education.schools.length; i++) {
-            var $start = $(HTMLschoolStart);
-            var school = education.schools[i];
-
-            $start.append(HTMLschoolName.replace('%data%', school.name).replace('#', school.url)
-                + HTMLschoolDegree.replace('%data%', school.degree));
-
-            $start.append(HTMLschoolDates.replace('%data%', school.dates));
-            $start.append(HTMLschoolLocation.replace('%data%', school.location));
-            $start.append(HTMLschoolMajor.replace('%data%', school.majors));
-
-            $education.append($start);
-        }
-    }
-
-    if (education.onlineCourses) {
-        $education.append(HTMLonlineClasses);
-
-        for (i = 0; i < education.onlineCourses.length; i++) {
-            var $startOnline = $(HTMLschoolStart);
-            var course = education.onlineCourses[i];
-
-            $startOnline.append(HTMLonlineTitle.replace('%data%', course.title).replace('#', course.url)
-            + HTMLonlineSchool.replace('%data%', course.school));
-            $startOnline.append(HTMLonlineDates.replace('%data%', course.date));
-            //$startOnline.append(HTMLonlineURL.replace('%data%', course.url));
-            $startOnline.append($('<br>'));
-
-            $education.append($startOnline);
-        }
-    }
+    var template = Handlebars.compile($("#educationTemplate").html());
+    var educationEntries = template(this);
+    $('#education').append(educationEntries);
 };
 
 
